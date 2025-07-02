8af7a0f0f64eca8f4e51e54059e89117fcfaeaad
Aditya Khatri <adityakhatri47@gmail.com>
2025-07-02 15:54:04 +0545

    fix(build): update stylelint no unused vars

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-        'stylelint-no-unused-selectors',
+        '@togglecorp/stylelint-no-unused-selectors',
         'stylelint-value-no-unknown-custom-properties',
     ],
     extends: [
@@ -11,14 +11,14 @@ module.exports = {
         // https://stylelint.io/migration-guide/to-15
         // indentation: 4,
         'plugin/no-unused-selectors': {
-            'suffixesToStrip': ['.module'],
-            'documents': [
+            suffixesToStrip: ['.module'],
+            documents: [
                 '{cssDir}/{cssName}.tsx',
             ],
         },
         'csstools/value-no-unknown-custom-properties': [
             true, {
-                importFrom: ['./src/index.css']
+                importFrom: ['./src/index.css'],
             },
         ],
         'selector-pseudo-class-no-unknown': [
