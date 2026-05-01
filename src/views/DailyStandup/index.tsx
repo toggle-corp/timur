da197132994cfca5fbf6ee39a90224685e8cc748
tnagorra <weathermist@gmail.com>
2026-05-01 14:44:27 +0545

    fix: fix knip issues

--- src/views/DailyStandup/index.tsx
+++ src/views/DailyStandup/index.tsx
@@ -57,7 +57,6 @@ const ALL_PROJECTS = gql`
     }
 `;
 
-/** @knipignore */
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
     const { date: dateFromParams } = useParams<{ date: string | undefined}>();
